1070c6c3f56a0194236422425381e3fece2b3448
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-21 01:28:00 +0530

    Update PutMoney.jsx

--- src/sections/PutMoney.jsx
+++ src/sections/PutMoney.jsx
@@ -5,18 +5,18 @@ function PutMoney() {
   return (
     <section>
       <div className="container-wrapper">
-        <div className=" flex  flex-col sm:flex-row justify-between gap-6">
+        <div className=" flex  flex-col sm:flex-row justify-between gap-6 items-center">
          <div className=" max-w-lg w-full">
         
         <PresaleBox/>
          </div>
 
           <div>
-            <img src="images/logo.png" alt="" />
+            <img src="images/logo.png" alt="" className=" mb-4" />
 
-            <h2>Put your money to work</h2>
+            <h2 className=" font-sohaBold text-5xl py-2 mb-4">Put your money <br /> to work</h2>
 
-            <p>
+            <p className=" max-w-2xl">
               LeedixPay is a secure and fast point of sale for physical stores
               and online shops that accepts Ethereum and many other coins and
               instantly converts them to Dai stablecoin so you don’t have to
