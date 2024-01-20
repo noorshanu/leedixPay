94b3082dffe58824dbf043dbcbfeb26cd4075617
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-20 23:19:11 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,10 @@ export default function Navbar() {
   return (
     <nav className="  ">
       <header className=" relative">
-        <div className="container-wrapper h-[72px] flex items-center justify-between">
+        <div
+          className="container-wrapper h-[72px] flex items-center justify-between pt-0 sm:pt-8
+        "
+        >
           <Link to="/">
             <img
               src="/images/logo.png"
@@ -36,87 +39,48 @@ export default function Navbar() {
 
           <aside
             className={twMerge(
-              "max-lg:fixed bottom-0 right-0 max-lg:w-[320px] max-lg:h-[calc(100vh-72px)] max-lg:bg-text  flex flex-col lg:flex-row lg:items-center lg:space-x-10 max-lg:text-white lg:[&>*]:text-black/60 [&>*]:tracking-[1px] max-lg:p-5 max-lg:[&>a]:py-2 max-lg:[&>a]:border-b max-lg:[&>a]:px-3 max-lg:[&>a]:mb-2 max-lg:[&>a]:uppercase transition-all duration-200 translate-x-[320px] lg:translate-x-0 z-[1000] max-lg:overflow-y-auto",
+              "max-lg:fixed bottom-0 right-0 max-lg:w-[320px] max-lg:h-[calc(100vh-12px)] max-lg:bg-white   flex flex-col lg:flex-row lg:items-center lg:space-x-10 max-lg:text-black lg:[&>*]:text-black/60 [&>*]:tracking-[1px] max-lg:p-5 max-lg:[&>a]:py-2 max-lg:[&>a]:border-b max-lg:[&>a]:px-3 max-lg:[&>a]:mb-2 max-lg:[&>a]:uppercase transition-all duration-200 translate-x-[320px] lg:translate-x-0 z-[1000] max-lg:overflow-y-auto",
               isSidebarOpen ? "translate-x-0" : null
             )}
           >
             <Typography
               as={Link}
               variant="sm"
-              className="font-medium cursor-pointer"
+              className="font-medium cursor-pointer text-lg font-sohaMed"
               onClick={() => setSidebarVisibility(false)}
             >
-              Staking
+              Home
             </Typography>
             <Typography
-              onClick={closeSidebar}
-              id="about"
+              as={Link}
               variant="sm"
-              className="font-medium cursor-pointer"
+              className="font-medium cursor-pointer text-lg font-sohaMed"
+              onClick={() => setSidebarVisibility(false)}
             >
               About
             </Typography>
-
             <Typography
-              as="a"
-              onClick={closeSidebar}
-              id="how-to-buy"
-              href=""
+              as={Link}
               variant="sm"
-              className="font-medium cursor-pointer"
+              className="font-medium cursor-pointer text-lg font-sohaMed"
+              onClick={() => setSidebarVisibility(false)}
             >
-              How To Buy
+              Services
+            </Typography>
+            <Typography
+              as={Link}
+              variant="sm"
+              className="font-medium cursor-pointer text-lg font-sohaMed"
+              onClick={() => setSidebarVisibility(false)}
+            >
+              Login
             </Typography>
 
-            <Typography
-              as="a"
-              onClick={closeSidebar}
-              id="roadmap"
-              href="/"
-              variant="sm"
-              className="font-medium cursor-pointer"
-            >
-              Roadmap
-            </Typography>
-
-            <Typography
-              onClick={closeSidebar}
-              id="faqs"
-              to=""
-              variant="sm"
-              className="font-medium cursor-pointer"
-            >
-              FAQS
-            </Typography>
-
-            <button className=" btn-nav rounded-md text-sm h-10 mt-4 mb-6 text-white ">
-              Buy ETHETF Token
+            <button className=" btn-nav  px-2 sm:px-4 py-3  ">
+              <span className="text-white font-sohaBold font-bold text-base sm:text-xl">
+                REGISTER NOW
+              </span>
             </button>
-
-            <div className="flex max-lg:space-x-8 space-x-4 justify-center max-lg:[&>*]:invert">
-              <a
-                rel="noreferrer"
-                href="https://x.com/ethetf_token"
-                target="_blank"
-              >
-                <img
-                  src="/images/icons/twitter.svg"
-                  className="max-lg:h-8 h-6"
-                  alt=""
-                />
-              </a>
-              <a
-                rel="noreferrer"
-                href="https://t.me/ETHETF_Token"
-                target="_blank"
-              >
-                <img
-                  src="/images/icons/telegram.svg"
-                  className="max-lg:h-8 h-6"
-                  alt=""
-                />
-              </a>
-            </div>
           </aside>
         </div>
       </header>
