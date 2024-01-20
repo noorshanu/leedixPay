0b13246aa4544c11a3e9a6f479620ff3a99d451c
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-21 01:33:13 +0530

    Update PutMoney.jsx

--- src/sections/PutMoney.jsx
+++ src/sections/PutMoney.jsx
@@ -5,7 +5,7 @@ function PutMoney() {
   return (
     <section>
       <div className="container-wrapper">
-        <div className=" flex  flex-col sm:flex-row justify-between gap-6 items-center">
+        <div className=" flex  flex-col sm:flex-row justify-between gap-2 items-center">
          <div className=" max-w-lg w-full">
         
         <PresaleBox/>
@@ -16,19 +16,19 @@ function PutMoney() {
 
             <h2 className=" font-sohaBold text-5xl py-2 mb-4">Put your money <br /> to work</h2>
 
-            <p className=" max-w-2xl">
+            <p className=" max-w-5xl font-sohaReg text-lg">
               LeedixPay is a secure and fast point of sale for physical stores
-              and online shops that accepts Ethereum and many other coins and
-              instantly converts them to Dai stablecoin so you don’t have to
+              and online shops that <br />accepts Ethereum and many other coins and
+              instantly converts them to Dai <br /> stablecoin so you don’t have to
               worry about volatility. CONTACT US
             </p>
 
-            <div className=" flex">
-                <p>
+            <div className=" flex gap-4 items-center mt-14">
+                <p className=" font-sohaReg text-3xl">
                 How to buy LDP
                 </p>
 
-                <button> Whitepaper</button>
+                <button className="btn-hero px-12 py-2 font-sohaBold text-lg text-white"> Whitepaper</button>
             </div>
           </div>
         </div>
