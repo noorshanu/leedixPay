9d8f5077b3bdbd8054d09e3f84fca9d3afa6ff74
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-21 01:52:39 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop ";
 
 import Home from "./Pages/Home";
+import AboutUs from "./Pages/AboutUs";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <div className="">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about-us" element={<AboutUs/>} />
           </Routes>
         </div>
       </Router>
