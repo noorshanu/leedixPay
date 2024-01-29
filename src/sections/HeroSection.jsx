78f87e2c9fabe849ff535cca71ae5f713f3465ec
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-30 00:09:44 +0530

    Update HeroSection.jsx

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -13,7 +13,7 @@ export default function HeroSection() {
               <span className=" font-sohaBold lh-0-5">Bringing Dreams to Life, One Transfer at a Time</span>
             </Typography>
 
-            <Typography className=" xl:text-[18px] font-medium font-sohaReg lh-1_3  mb-8">
+            <Typography className=" xl:text-[0.9rem] font-medium font-sohaReg lh-1_9  mb-8">
               LeedixPay is a secure and fast point of sale for physical stores
               and online shops that accepts Ethereum and many other coins and
               instantly converts them to Dai stablecoin so you don’t have to
